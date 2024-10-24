d0386ac1a416d57498b68cdf9508a288987d07bc
aribambang <aribk210@gmail.com>
2024-10-24 15:29:16 +0700

    add column KiloCalories on table Dish

--- Ringmaster.Infrastructure/Migrations/20241024080958_KiloCaloriesAddedToDish.Designer.cs
+++ Ringmaster.Infrastructure/Migrations/20241024080958_KiloCaloriesAddedToDish.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Ringmaster.Infrastructure.Persistance;
@@ -11,9 +12,11 @@ using Ringmaster.Infrastructure.Persistance;
 namespace Ringmaster.Infrastructure.Migrations
 {
     [DbContext(typeof(RestaurantsDBContext))]
-    partial class RestaurantsDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241024080958_KiloCaloriesAddedToDish")]
+    partial class KiloCaloriesAddedToDish
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Ringmaster.Infrastructure/Migrations/20241024080958_KiloCaloriesAddedToDish.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ringmaster.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class KiloCaloriesAddedToDish : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KiloCalories",
+                table: "Dish",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KiloCalories",
+                table: "Dish");
+        }
+    }
+}
